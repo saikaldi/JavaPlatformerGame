9f8d295dd19a72ff56f31a118ab244aee51e56e1
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-12-02 09:58:41 +0100

    Added animation for image

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -9,19 +9,26 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Random;
+import static utilz.Constant.PlayerConstants.*;
+import static utilz.Constant.Directions.*;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs; // an instance of a class that implements the MouseListener interface
     private float xDelta = 100, yDelta = 100;
-    private BufferedImage img, subImg;
+    private BufferedImage img;
+    private BufferedImage[][] animations;
+    private int aniTick, aniIndex, aniSpeed = 15;
+    private int playerAction = IDLE;
+    private int playerDir = -1;
+    private boolean moving = false;
 
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this); // Initialize mouse input handling
 
         importImg();
+        loadAnimations();
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         // This is a method from a JComponent (like JPanel or JFrame) that registers
@@ -30,12 +37,25 @@ public class GamePanel extends JPanel {
         addMouseMotionListener(mouseInputs);
     }
 
+    private void loadAnimations() {
+        animations = new BufferedImage[9][6];
+        for (int j = 0; j < animations.length; j++)
+            for (int i = 0; i < animations[j].length; i++)
+                animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
+    }
+
     public void importImg(){
         InputStream is = getClass().getResourceAsStream("/player.png");
         try {
             img = ImageIO.read(is);
         }catch (IOException e){
             e.printStackTrace();
+        }finally{
+            try{
+                is.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
@@ -46,17 +66,59 @@ public class GamePanel extends JPanel {
         setPreferredSize(size);
     }
 
-    public void changeXDelta(int value){
-        this.xDelta += value;
+
+    public void  setDirection(int direction) {
+        this.playerDir = direction;
+        moving = true;
+    }
+
+    public void  setMoving(boolean moving){
+        this.moving = moving;
+
     }
 
     public void changeYDelta(int value){
         this.yDelta += value;
     }
 
-    public void setRect(int x, int y){
-        this.xDelta = x;
-        this.yDelta = y;
+    private void updateAnimationTick(){ // Updates animation frame based on timing
+
+        aniTick++; // Increment animation tick counter
+        if(aniTick >= aniSpeed){ // If tick count reaches animation speed threshold
+            aniTick = 0; // Reset tick counter
+            aniIndex++; // Move to the next frame
+            if(aniIndex >= GetSpriteAmount(playerAction)){ // If end of animation frames is reached
+                aniIndex = 0; // Reset frame index to loop animation
+            }
+        }
+    }
+
+
+
+    private void setAnimation() {
+        if(moving)
+            playerAction = RUNNING;
+        else
+            playerAction = IDLE;
+    }
+
+    private void updatePos() {
+        if(moving){
+            switch (playerDir){
+                case LEFT:
+                    xDelta -= 5;
+                    break;
+                case UP:
+                    yDelta -= 5;
+                    break;
+                case RIGHT:
+                    xDelta += 5;
+                    break;
+                case DOWN:
+                    yDelta += 5;
+                    break;
+            }
+        }
     }
 
 
@@ -67,11 +129,11 @@ public class GamePanel extends JPanel {
 //        JComponent is the superclass of JPanel
 //        public class JPanel extends JComponent implements Accessible
         super.paintComponent(g);
-
-        subImg = img.getSubimage(1*64, 8*40, 64, 40);
-        g.drawImage(subImg, (int)xDelta, (int)yDelta,128, 80, null);
+        setAnimation();
+        updateAnimationTick();
+        updatePos();
+        g.drawImage(animations[playerAction][aniIndex], (int)xDelta, (int)yDelta,128, 80, null);
     }
-
 
 
 
